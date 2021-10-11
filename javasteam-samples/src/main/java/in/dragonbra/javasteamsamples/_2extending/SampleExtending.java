0b474b9ccb772e36fc12c74e56450374d848309a
LossyDragon <thedragon1012@gmail.com>
2021-10-11 15:06:10 -0500

    Added more samples.

---
+++ javasteam-samples/src/main/java/in/dragonbra/javasteamsamples/_2extending/SampleExtending.java
@@ -0,0 +1,166 @@
+package in.dragonbra.javasteamsamples._2extending;
+
+import in.dragonbra.javasteam.enums.EResult;
+import in.dragonbra.javasteam.steam.handlers.steamuser.LogOnDetails;
+import in.dragonbra.javasteam.steam.handlers.steamuser.SteamUser;
+import in.dragonbra.javasteam.steam.handlers.steamuser.callback.LoggedOffCallback;
+import in.dragonbra.javasteam.steam.handlers.steamuser.callback.LoggedOnCallback;
+import in.dragonbra.javasteam.steam.steamclient.SteamClient;
+import in.dragonbra.javasteam.steam.steamclient.callbackmgr.CallbackManager;
+import in.dragonbra.javasteam.steam.steamclient.callbacks.ConnectedCallback;
+import in.dragonbra.javasteam.steam.steamclient.callbacks.DisconnectedCallback;
+import in.dragonbra.javasteam.util.log.DefaultLogListener;
+import in.dragonbra.javasteam.util.log.LogManager;
+
+//
+// Sample 2: Extending SteamKit2
+//
+// this sample introduces the method through which SK2 can be extended
+// with custom message handling and additional features
+//
+// this sample extends Sample 1 by making use of a custom handler and a custom callback
+//
+// of interest are the calls to SteamClient.AddHandler and the MyHandler.java file
+//
+
+/**
+ * @author lngtr
+ * @since 2021-10-11
+ */
+@SuppressWarnings("Duplicates")
+public class SampleExtending implements Runnable {
+
+    private SteamClient steamClient;
+
+    private CallbackManager manager;
+
+    private SteamUser steamUser;
+
+    private MyHandler myHandler;
+
+    private boolean isRunning;
+
+    private String user;
+
+    private String pass;
+
+    public SampleExtending(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Sample2: No username and password specified!");
+            return;
+        }
+
+        LogManager.addListener(new DefaultLogListener());
+
+        new SampleExtending(args[0], args[1]).run();
+    }
+
+    @Override
+    public void run() {
+
+        // create our steamclient instance
+        steamClient = new SteamClient();
+
+        steamClient.addHandler(new MyHandler());
+
+        // create the callback manager which will route callbacks to function calls
+        manager = new CallbackManager(steamClient);
+
+        // get the steamuser handler, which is used for logging on after successfully connecting
+        steamUser = steamClient.getHandler(SteamUser.class);
+
+        // now get an instance of our custom handler
+        myHandler = steamClient.getHandler(MyHandler.class);
+
+        // register a few callbacks we're interested in
+        // these are registered upon creation to a callback manager, which will then route the callbacks
+        // to the functions specified
+        manager.subscribe(ConnectedCallback.class, this::onConnected);
+        manager.subscribe(DisconnectedCallback.class, this::onDisconnected);
+
+        manager.subscribe(LoggedOnCallback.class, this::onLoggedOn);
+        manager.subscribe(LoggedOffCallback.class, this::onLoggedOff);
+
+        // handle our own custom callback
+        manager.subscribe(MyHandler.MyCallback.class, this::onMyCallback);
+
+        isRunning = true;
+
+        System.out.println("Connecting to steam...");
+
+        // initiate the connection
+        steamClient.connect();
+
+        // create our callback handling loop
+        while (isRunning) {
+            // in order for the callbacks to get routed, they need to be handled by the manager
+            manager.runWaitCallbacks(1000L);
+        }
+    }
+
+    private void onConnected(ConnectedCallback callback) {
+        System.out.println("Connected to Steam! Logging in " + user + "...");
+
+        LogOnDetails details = new LogOnDetails();
+        details.setUsername(user);
+        details.setPassword(pass);
+
+        // Set LoginID to a non-zero value if you have another client connected using the same account,
+        // the same private ip, and same public ip.
+        details.setLoginID(149);
+
+        steamUser.logOn(details);
+    }
+
+    private void onDisconnected(DisconnectedCallback callback) {
+        System.out.println("Disconnected from Steam");
+
+        isRunning = false;
+    }
+
+    private void onLoggedOn(LoggedOnCallback callback) {
+        if (callback.getResult() != EResult.OK) {
+            if (callback.getResult() == EResult.AccountLogonDenied) {
+                // if we receive AccountLogonDenied or one of it's flavors (AccountLogonDeniedNoMailSent, etc)
+                // then the account we're logging into is SteamGuard protected
+                // see sample 5 for how SteamGuard can be handled
+                System.out.println("Unable to logon to Steam: This account is SteamGuard protected.");
+
+                isRunning = false;
+                return;
+            }
+
+            System.out.println("Unable to logon to Steam: " + callback.getResult() + " / " + callback.getExtendedResult());
+
+            isRunning = false;
+            return;
+
+        }
+
+        System.out.println("Successfully logged on!");
+
+        // at this point, we'd be able to perform actions on Steam
+
+        // for this sample we'll just log off
+        // steamUser.logOff();
+    }
+
+    private void onLoggedOff(LoggedOffCallback callback) {
+        System.out.println("Logged off of Steam: " + callback.getResult());
+
+        isRunning = false;
+    }
+
+    private void onMyCallback(MyHandler.MyCallback callback) {
+        // this will be called when our custom callback gets posted
+        System.out.println("onMyCallback: " + callback.getResult());
+
+        // for this sample we'll just log off
+        myHandler.logOff("", "");
+    }
+}
--- javasteam-samples/src/main/java/in/dragonbra/javasteamsamples/_5steamguard/SampleSteamGuardRememberMe.java
+++ javasteam-samples/src/main/java/in/dragonbra/javasteamsamples/_5steamguard/SampleSteamGuardRememberMe.java
@@ -1,4 +1,4 @@
-package in.dragonbra.javasteamsamples;
+package in.dragonbra.javasteamsamples._5steamguard;
 
 import in.dragonbra.javasteam.enums.EResult;
 import in.dragonbra.javasteam.steam.handlers.steamuser.LogOnDetails;
@@ -23,6 +23,39 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
+//
+// Sample 5: SteamGuard
+//
+// this sample goes into detail for how to handle steamguard protected accounts and how to login to them
+//
+// SteamGuard works by enforcing a two factor authentication scheme
+// upon first logon to an account with SG enabled, the steam server will email an authcode to the validated address of the account
+// this authcode token can be used as the second factor during logon, but the token has a limited time span in which it is valid
+//
+// after a client logs on using the authcode, the steam server will generate a blob of random data that the client stores called a "sentry file"
+// this sentry file is then used in all subsequent logons as the second factor
+// ownership of this file provides proof that the machine being used to logon is owned by the client in question
+//
+// the usual login flow is thus:
+// 1. connect to the server
+// 2. logon to account with only username and password
+// at this point, if the account is steamguard protected, the LoggedOnCallback will have a result of AccountLogonDenied
+// the server will disconnect the client and email the authcode
+//
+// the login flow must then be restarted:
+// 1. connect to server
+// 2. logon to account using username, password, and authcode
+// at this point, login wil succeed and a UpdateMachineAuthCallback callback will be posted with the sentry file data from the steam server
+// the client will save the file, and reply to the server informing that it has accepted the sentry file
+//
+// all subsequent logons will use this flow:
+// 1. connect to server
+// 2. logon to account using username, password, and sha-1 hash of the sentry file
+
+/**
+ * @author lngtr
+ * @since 2018-02-28
+ */
 @SuppressWarnings("Duplicates")
 public class SampleSteamGuardRememberMe implements Runnable {
 
@@ -153,12 +186,16 @@ public class SampleSteamGuardRememberMe implements Runnable {
             }
 
             steamClient.disconnect();
+
             return;
         }
+
         if (callback.getResult() != EResult.OK) {
             System.out.println("Unable to logon to Steam: " + callback.getResult() + " / " + callback.getExtendedResult());
+
             isRunning = false;
             steamClient.disconnect();
+
             return;
         }
 
@@ -169,6 +206,7 @@ public class SampleSteamGuardRememberMe implements Runnable {
 
     private void onLoggedOff(LoggedOffCallback callback) {
         System.out.println("Logged off of Steam: " + callback.getResult());
+
         isRunning = false;
     }
 
