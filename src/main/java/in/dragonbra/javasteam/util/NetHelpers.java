f30455c1645be42dd1266bb31635c5d9e1b94365
Lossy <thedragon1012@gmail.com>
2022-01-15 04:14:45 -0600

    Add ServerRecord.TryCreateSocketServer()

--- src/main/java/in/dragonbra/javasteam/util/NetHelpers.java
+++ src/main/java/in/dragonbra/javasteam/util/NetHelpers.java
@@ -1,6 +1,9 @@
 package in.dragonbra.javasteam.util;
 
+import org.apache.commons.validator.routines.InetAddressValidator;
+
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
@@ -26,5 +29,28 @@ public class NetHelpers {
     public static int getIPAddress(InetAddress ip) {
         final ByteBuffer buff = ByteBuffer.wrap(ip.getAddress());
         return (int) (buff.getInt() & 0xFFFFFFFFL);
+    }
+
+
+    public static InetSocketAddress tryParseIPEndPoint(String address) {
+        if (address == null) {
+            return null;
+        }
+
+        String[] split = address.split(":");
+
+        if (!InetAddressValidator.getInstance().isValidInet4Address(split[0])) {
+            return null;
+        }
+
+        try {
+            if (split.length > 1) {
+                return new InetSocketAddress(split[0], Integer.parseInt(split[1]));
+            }
+        } catch (IllegalArgumentException exception) {
+            // no-op
+        }
+
+        return null;
     }
 }
--- src/test/java/in/dragonbra/javasteam/steam/discovery/ServerRecordTest.java
+++ src/test/java/in/dragonbra/javasteam/steam/discovery/ServerRecordTest.java
@@ -4,6 +4,9 @@ import in.dragonbra.javasteam.TestBase;
 import in.dragonbra.javasteam.networking.steam3.ProtocolTypes;
 import org.junit.Test;
 
+import java.net.InetSocketAddress;
+import java.util.EnumSet;
+
 import static org.junit.Assert.*;
 
 /**
@@ -54,5 +57,40 @@ public class ServerRecordTest extends TestBase {
         assertTrue(r.equals(l));
 
         assertEquals(l.hashCode(), r.hashCode());
+    }
+
+    @Test
+    public void canTryCreateSocketServer() {
+        ServerRecord record = ServerRecord.tryCreateSocketServer("127.0.0.1:1234");
+
+        assertNotNull(record);
+        assertEquals(new InetSocketAddress("127.0.0.1", 1234), record.getEndpoint());
+        assertEquals(EnumSet.of(ProtocolTypes.TCP, ProtocolTypes.UDP), record.getProtocolTypes());
+
+        record = ServerRecord.tryCreateSocketServer("192.168.0.1:5678");
+
+        assertNotNull(record);
+        assertEquals(new InetSocketAddress("192.168.0.1", 5678), record.getEndpoint());
+        assertEquals(EnumSet.of(ProtocolTypes.TCP, ProtocolTypes.UDP), record.getProtocolTypes());
+    }
+
+    @Test
+    public void cannotTryCreateSocketServer() {
+        ServerRecord record;
+
+        record = ServerRecord.tryCreateSocketServer("127.0.0.1");
+        assertNull(record);
+
+        record = ServerRecord.tryCreateSocketServer("127.0.0.1:123456789");
+        assertNull(record);
+
+        record = ServerRecord.tryCreateSocketServer("127.0.0.1:-1234");
+        assertNull(record);
+
+        record = ServerRecord.tryCreateSocketServer("127.0.0.1:notanint");
+        assertNull(record);
+
+        record = ServerRecord.tryCreateSocketServer("volvopls.valvesoftware.com:1234");
+        assertNull(record);
     }
 }
