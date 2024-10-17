90f4a5a30b3c22e15f0a6c5f37a8828a7196b10d
LossyDragon <thedragon1012@gmail.com>
2024-10-17 00:11:48 -0500

    Add tests in UtilsTest

--- src/test/java/in/dragonbra/javasteam/util/UtilsTest.java
+++ src/test/java/in/dragonbra/javasteam/util/UtilsTest.java
@@ -1,8 +1,7 @@
 package in.dragonbra.javasteam.util;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author lngtr
@@ -17,6 +16,27 @@ public class UtilsTest {
     @Test
     public void crc32() {
         long result = Utils.crc32("test_string");
-        assertEquals(0x0967B587, result);
+        Assertions.assertEquals(0x0967B587, result);
+    }
+
+    @Test
+    public void toHex() {
+        byte[] byteArray = {(byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF};
+        Assertions.assertEquals("DEADBEEF", Strings.toHex(byteArray));
+    }
+    @Test
+    public void decodeHex() {
+        byte[] byteArray = {(byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF};
+        Assertions.assertArrayEquals(byteArray, Strings.decodeHex("deadbeef"));
+    }
+    @Test
+    public void invalid_decodeHex() {
+        Assertions.assertThrows(StringIndexOutOfBoundsException.class, () -> Strings.decodeHex("odd"));
+    }
+    @Test
+    public void isNullOrEmpty() {
+        Assertions.assertTrue(Strings.isNullOrEmpty(null));
+        Assertions.assertTrue(Strings.isNullOrEmpty(""));
+        Assertions.assertFalse(Strings.isNullOrEmpty("Hello World!"));
     }
 }
