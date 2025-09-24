df41066b5e17c4379c8b13e3c4b7194a8a386e52
LossyDragon <thedragon1012@gmail.com>
2025-09-23 21:58:22 -0500

    add xz dependency for samples

--- javasteam-samples/build.gradle.kts
+++ javasteam-samples/build.gradle.kts
@@ -21,4 +21,5 @@ dependencies {
     implementation(libs.protobuf.java) // To access protobufs directly as shown in Sample #2
     implementation(libs.qrCode)
     implementation(libs.zstd) // Content Downloading.
+    implementation(libs.xz) // Content Downloading.
 }
