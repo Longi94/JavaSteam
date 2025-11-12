af8aead22e29b1aa43ec6c05de10e0aa7d5995b3
LossyDragon <thedragon1012@gmail.com>
2025-11-12 13:34:53 -0600

    Add dota protos

---
+++ javasteam-dota2/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.protobuf.gradle)
+    id("java")
+    id("maven-publish")
+    id("signing")
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+    targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
+    withSourcesJar()
+    withJavadocJar()
+}
+
+/* Protobufs */
+protobuf.protoc {
+    artifact = libs.protobuf.protoc.get().toString()
+}
+
+/* Java Docs */
+tasks.javadoc {
+    exclude("**/in/dragonbra/javasteam/protobufs/**")
+}
+
+dependencies {
+    implementation(libs.protobuf.java)
+}
+
+/* Artifact publishing */
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name = "JavaSteam-dota2"
+                packaging = "jar"
+                description = "Dota classes for JavaSteam."
+                url = "https://github.com/Longi94/JavaSteam"
+                inceptionYear = "2025"
+                scm {
+                    connection = "scm:git:git://github.com/Longi94/JavaSteam.git"
+                    developerConnection = "scm:git:ssh://github.com:Longi94/JavaSteam.git"
+                    url = "https://github.com/Longi94/JavaSteam/tree/master"
+                }
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://www.opensource.org/licenses/mit-license.php"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Longi"
+                        name = "Long Tran"
+                        email = "lngtrn94@gmail.com"
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "javasteam"
 include(":javasteam-samples")
-include(":javasteam-tf")
 include(":javasteam-cs")
-
+include(":javasteam-tf")
 include("javasteam-deadlock")
+include("javasteam-dota2")
